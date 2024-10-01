c2fb71a30c7385f345da39bb8a09a7a5dbcabd06
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:23:59 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 import os
 
 def estimativa():
- engine = create_engine(os.environ['banco_sql_postgresql'])
+ engine = create_engine(os.getenv('banco_sql_postgresql'))
  query = """
  SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
  FROM orders
@@ -19,6 +19,6 @@ def estimativa():
  sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
  ax.set_title('Faturamento Mensal de 2023')
  ax.set_xlabel('Mes')
- ax.set_ylabel('Faturamento')
+ ax.set_ylabel('Faturamento (R$)')
  plt.xticks(rotation=45)
  return fig
