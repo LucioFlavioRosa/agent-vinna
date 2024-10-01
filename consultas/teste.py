be04151e7c8b7d8c5ff706aaef1e7ff31114095b
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:46:05 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,23 +1,23 @@
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
     query = """
-    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    SELECT DATE(data_da_compra) AS data_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
     FROM orders
-    WHERE DATE_PART('year', data_da_compra) = 2023
-    GROUP BY mes
-    ORDER BY mes;
+    WHERE DATE(data_da_compra) BETWEEN '2023-01-01' AND '2023-12-31'
+    GROUP BY DATE(data_da_compra)
+    ORDER BY DATE(data_da_compra);
     """
     df = pd.read_sql_query(query, engine)
     fig, ax = plt.subplots()
-    sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
-    ax.set_title('Faturamento Mensal de 2023')
-    ax.set_xlabel('Mes')
+    sns.lineplot(data=df, x='data_compra', y='faturamento', ax=ax)
+    ax.set_title('Faturamento de 2023')
+    ax.set_xlabel('Data')
     ax.set_ylabel('Faturamento')
     return fig
