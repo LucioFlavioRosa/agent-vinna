6d7d2408bb8b1f0d774c845307ad359133f33e77
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:06:16 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -7,24 +7,33 @@ import matplotlib.pyplot as plt
 import os
 
 def estimativa():
-    engine = create_engine(os.environ['banco_sql_postgresql'])
-    query_orders = "SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) as faturamento FROM orders WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-08-01' GROUP BY data_da_compra ORDER BY data_da_compra"
+    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    
+    query_orders = """
+    SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    FROM orders
+    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
+    GROUP BY data_da_compra
+    ORDER BY data_da_compra
+    """
+    
     df_orders = pd.read_sql_query(query_orders, engine)
-
+    
     df_orders['data_da_compra'] = pd.to_datetime(df_orders['data_da_compra'])
-    df_orders.rename(columns={'data_da_compra': 'ds', 'faturamento': 'y'}, inplace=True)
-
+    df_orders = df_orders.rename(columns={'data_da_compra': 'ds', 'faturamento': 'y'})
+    
     model = Prophet()
     model.fit(df_orders)
-
-    future = model.make_future_dataframe(periods=30)
-    forecast = model.predict(future)
-
-    monthly_forecast = forecast[['ds', 'yhat']].set_index('ds').resample('M').sum()
-
-    plt.figure(figsize=(10, 6))
-    plt.pie(monthly_forecast['yhat'], labels=monthly_forecast.index.strftime('%Y-%m'), autopct='%1.1f%%')
-    plt.title('Faturamento Mensal Estimado de Janeiro a Julho de 2024')
-    plt.savefig('resultado_python/faturamento_estimado.png')
     
-    return 'faturamento_estimado.png'
+    future = model.make_future_dataframe(periods=180)
+    forecast = model.predict(future)
+    
+    monthly_faturamento = forecast[['ds', 'yhat']].set_index('ds').resample('M').sum()
+    monthly_faturamento.reset_index(inplace=True)
+    
+    plt.figure(figsize=(10, 6))
+    plt.pie(monthly_faturamento['yhat'], labels=monthly_faturamento['ds'].dt.strftime('%Y-%m'), autopct='%1.1f%%')
+    plt.title('Faturamento Mensal Estimado de Janeiro a Julho de 2024')
+    plt.savefig('resultado_python/grafico_faturamento.png')
+    
+    return 'grafico_faturamento.png'
