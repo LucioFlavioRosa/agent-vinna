5a9f1681544d132460824f1668e777dbc2df43c2
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 10:07:43 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,17 +1,17 @@
 
 import pandas as pd
-import sqlalchemy
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine
 import os
 
 def estimativa():
-    engine = sqlalchemy.create_engine(os.getenv('banco_sql_postgresql'))
-    orders = pd.read_sql_query("SELECT * FROM orders WHERE data_da_compra >= '2024-07-01' AND data_da_compra < '2024-08-01'", engine)
-    products = pd.read_sql_query("SELECT * FROM products", engine)
-    merged_data = pd.merge(orders, products, on='id_produto')
-    faturamento = merged_data.groupby(['subgrupo_do_produto'])['preco_unitario', 'quantidade_do_produto_vendida'].sum().reset_index()
-    faturamento['total'] = faturamento['preco_unitario'] * faturamento['quantidade_do_produto_vendida']
+    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    orders = pd.read_sql_query("SELECT * FROM orders", engine)
+    products = pd.read_sql_query("SELECT * FROM products WHERE grupo_do_produto = 'carro'", engine)
+    merged = pd.merge(orders, products, on='id_produto')
+    merged['faturamento'] = merged['preco_unitario'] * merged['quantidade_do_produto_vendida']
+    faturamento_por_subgrupo = merged.groupby('subgrupo_do_produto')['faturamento'].sum().reset_index()
     fig, ax = plt.subplots()
-    sns.pieplot(data=faturamento, x='total', y='subgrupo_do_produto', ax=ax)
+    sns.pieplot(data=faturamento_por_subgrupo, x='faturamento', labels=faturamento_por_subgrupo['subgrupo_do_produto'])
     return fig
