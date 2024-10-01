9164d9e408b05ebe43b04a9c32e7c6563c9c5d23
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:29:45 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 import os
 
 def estimativa():
- engine = create_engine(os.environ['banco_sql_postgresql'])
+ engine = create_engine(os.getenv('banco_sql_postgresql'))
  query = """
- SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida + valor_frete) AS faturamento
+ SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
  FROM orders
  WHERE DATE_PART('year', data_da_compra) = 2023
  GROUP BY mes
