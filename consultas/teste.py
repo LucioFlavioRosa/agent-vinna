81879813ae00bab890c0c959fbab177e2d0cf531
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 10:13:02 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,16 +1,17 @@
 
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sqlalchemy import create_engine
 import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    orders = pd.read_sql_query("SELECT id_produto, quantidade_do_produto_vendida FROM orders", engine)
+    orders = pd.read_sql_query("SELECT id_produto, preco_unitario, quantidade_do_produto_vendida FROM orders", engine)
     products = pd.read_sql_query("SELECT id_produto, grupo_do_produto FROM products", engine)
     merged_data = pd.merge(orders, products, on='id_produto')
-    faturamento = merged_data.groupby('grupo_do_produto').agg({'quantidade_do_produto_vendida': 'sum'}).reset_index()
+    merged_data['faturamento'] = merged_data['preco_unitario'] * merged_data['quantidade_do_produto_vendida']
+    faturamento_por_grupo = merged_data.groupby('grupo_do_produto')['faturamento'].sum().reset_index()
     fig, ax = plt.subplots()
-    sns.pieplot(data=faturamento, x='quantidade_do_produto_vendida', y='grupo_do_produto', ax=ax)
+    ax.pie(faturamento_por_grupo['faturamento'], labels=faturamento_por_grupo['grupo_do_produto'], autopct='%1.1f%%')
     return fig
