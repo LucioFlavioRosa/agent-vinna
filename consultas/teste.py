4268df3638a01da60c5cccb4d75635a9a25d749e
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:05:04 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,34 +1,27 @@
 
 import pandas as pd
-from sqlalchemy import create_engine
-from prophet import Prophet
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine
 import os
 
 def estimativa():
-    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    engine = create_engine(os.environ['banco_sql_postgresql'])
     query = """
-    SELECT DATE(data_da_compra) AS data, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
-    FROM orders
-    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-07-01'
-    GROUP BY DATE(data_da_compra)
-    ORDER BY data;
+    SELECT 
+        EXTRACT(MONTH FROM data_da_compra) AS mes,
+        SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    FROM 
+        orders
+    WHERE 
+        data_da_compra >= '2024-01-01' AND data_da_compra < '2024-08-01'
+    GROUP BY 
+        mes
+    ORDER BY 
+        mes;
     """
     df = pd.read_sql_query(query, engine)
-    df.columns = ['ds', 'y']
-    
-    model = Prophet()
-    model.fit(df)
-    
-    future = model.make_future_dataframe(periods=180)
-    forecast = model.predict(future)
-    
-    monthly_faturamento = forecast[['ds', 'yhat']].set_index('ds').resample('M').sum()
-    
-    plt.figure(figsize=(10, 6))
-    plt.pie(monthly_faturamento['yhat'], labels=monthly_faturamento.index.strftime('%Y-%m'), autopct='%1.1f%%')
-    plt.title('Faturamento Mensal de Janeiro a Julho de 2024')
-    
-    plt.savefig('resultado_python/grafico_faturamento.png')
-    return 'grafico_faturamento.png'
+    fig, ax = plt.subplots()
+    ax.pie(df['faturamento'], labels=df['mes'], autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')
+    return fig
