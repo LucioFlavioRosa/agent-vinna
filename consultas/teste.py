c4087581124360c3899fa247632ef27a4976c111
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 08:22:14 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,14 +1,27 @@
 
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
+from prophet import Prophet
 import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    orders = pd.read_sql_query("SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento FROM orders WHERE data_da_compra BETWEEN '2023-01-01' AND '2023-12-31' GROUP BY data_da_compra", engine)
-    orders['data_da_compra'] = pd.to_datetime(orders['data_da_compra'])
-    fig, ax = plt.subplots()
-    sns.lineplot(data=orders, x='data_da_compra', y='faturamento', ax=ax)
-    return fig
+    
+    query_orders = """
+    SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS total_vendas
+    FROM orders
+    WHERE data_da_compra >= NOW() - INTERVAL '1 year 6 months'
+    GROUP BY data_da_compra
+    ORDER BY data_da_compra
+    """
+    
+    df = pd.read_sql(query_orders, engine)
+    df.rename(columns={'data_da_compra': 'ds', 'total_vendas': 'y'}, inplace=True)
+    
+    model = Prophet()
+    model.fit(df)
+    
+    future = model.make_future_dataframe(periods=31)  # Previsão para 31 dias
+    forecast = model.predict(future)
+    
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(31)
