8dfe0f1e40a8fae14b55085ba6a1976d243646c1
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 10:35:21 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -6,10 +6,11 @@ import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    orders = pd.read_sql_query('SELECT id_produto, quantidade_do_produto_vendida FROM orders', engine)
-    products = pd.read_sql_query('SELECT id_produto, grupo_do_produto, subgrupo_do_produto FROM products', engine)
-    merged_data = pd.merge(orders, products, on='id_produto')
-    faturamento = merged_data.groupby(['grupo_do_produto', 'subgrupo_do_produto']).sum().reset_index()
+    orders = pd.read_sql_query('SELECT * FROM orders', engine)
+    products = pd.read_sql_query('SELECT * FROM products', engine)
+    merged = pd.merge(orders, products, on='id_produto')
+    faturamento = merged.groupby(['grupo_do_produto', 'subgrupo_do_produto']).agg({'preco_unitario': 'sum'}).reset_index()
+    plt.figure(figsize=(8, 8))
+    plt.pie(faturamento['preco_unitario'], labels=faturamento['subgrupo_do_produto'], autopct='%1.1f%%')
     fig, ax = plt.subplots()
-    ax.pie(faturamento['quantidade_do_produto_vendida'], labels=faturamento['subgrupo_do_produto'], autopct='%1.1f%%')
     return fig
