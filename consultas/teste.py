d1ed80552023be8e70a445eb7b2bfc51abeedd89
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:11:02 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,9 +1,9 @@
 
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
-import os
 
 def estimativa():
  engine = create_engine(os.getenv('banco_sql_postgresql'))
@@ -17,7 +17,4 @@ def estimativa():
  df = pd.read_sql_query(query, engine)
  sns.barplot(x='mes', y='faturamento', data=df)
  plt.xticks(rotation=45)
- plt.title('Faturamento Mensal de 2023')
- plt.xlabel('Mes')
- plt.ylabel('Faturamento')
  return plt.show()
