ce1c70281d66bd778b649273f253084c7b30406c
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 08:46:13 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -7,17 +7,12 @@ import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    query = """
-    SELECT DATE_TRUNC('month', o.data_da_compra) AS mes, SUM(o.preco_unitario * o.quantidade_do_produto_vendida) AS faturamento
-    FROM orders o
-    JOIN products p ON o.id_produto = p.id_produto
-    WHERE p.grupo_do_produto = 'padaria' AND EXTRACT(YEAR FROM o.data_da_compra) = 2023
-    GROUP BY mes
-    ORDER BY mes;
-    """
-    df = pd.read_sql_query(query, engine)
+    query_orders = "SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento FROM orders WHERE data_da_compra BETWEEN '2023-01-01' AND '2023-12-31' GROUP BY data_da_compra"
+    df = pd.read_sql_query(query_orders, engine)
+    df['mes'] = pd.to_datetime(df['data_da_compra']).dt.to_period('M')
+    faturamento_mensal = df.groupby('mes')['faturamento'].sum().reset_index()
     fig, ax = plt.subplots()
-    sns.barplot(data=df, x='mes', y='faturamento', ax=ax)
+    sns.barplot(x='mes', y='faturamento', data=faturamento_mensal, ax=ax)
     ax.set_title('Faturamento Mensal em 2023 do Grupo Padaria')
     ax.set_xlabel('Mes')
     ax.set_ylabel('Faturamento')
