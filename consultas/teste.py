c26f6cdffcc9e49d9349e659d72106f8279e83ed
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:33:21 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -10,14 +10,14 @@ def estimativa():
     query = """
     SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
     FROM orders
-    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
+    WHERE DATE_PART('year', data_da_compra) = 2023
     GROUP BY mes
     ORDER BY mes;
     """
     df = pd.read_sql_query(query, engine)
     fig, ax = plt.subplots()
-    sns.barplot(data=df, x='mes', y='faturamento', ax=ax)
+    sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
     ax.set_title('Faturamento Mensal de 2023')
     ax.set_xlabel('Mes')
-    ax.set_ylabel('Faturamento')
+    ax.set_ylabel('Faturamento (R$)')
     return fig
