2bdd626a62fe07735bbdbc7e275c3cd4feb8e073
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:32:49 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -16,27 +16,27 @@ def estimativa():
     products = pd.read_sql_query(query_products, engine)
     
     merged_data = pd.merge(orders, products, on='id_produto')
+    
     merged_data['faturamento'] = merged_data['preco_unitario'] * merged_data['quantidade_do_produto_vendida']
     
     faturamento_julho = merged_data.groupby(['subgrupo_do_produto'])['faturamento'].sum().reset_index()
     
-    faturamento_julho.columns = ['subgrupo', 'faturamento']
-    
-    model = Prophet()
-    faturamento_julho['ds'] = pd.date_range(start='2024-07-01', periods=len(faturamento_julho), freq='D')
+    faturamento_julho['ds'] = pd.to_datetime('2024-07-01')
     faturamento_julho['y'] = faturamento_julho['faturamento']
     
+    model = Prophet()
     model.fit(faturamento_julho[['ds', 'y']])
     
     future = model.make_future_dataframe(periods=30)
     forecast = model.predict(future)
     
     plt.figure(figsize=(10, 6))
-    sns.barplot(data=faturamento_julho, x='subgrupo', y='faturamento')
-    plt.title('Faturamento por Subgrupo - Julho 2024')
+    sns.barplot(data=faturamento_julho, x='subgrupo_do_produto', y='faturamento')
+    plt.title('Faturamento por Subgrupo - Julho de 2024')
     plt.xticks(rotation=45)
     plt.tight_layout()
     
     plt.savefig('resultado_python/faturamento_julho_2024.png')
+    plt.close()
     
     return 'faturamento_julho_2024.png'
