8f0baac0326442860deb516c9fadca46e09baf0a
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 08:56:32 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -7,12 +7,10 @@ import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    query_orders = "SELECT * FROM orders WHERE data_da_compra >= '2023-05-01' AND data_da_compra < '2023-06-01'"
-    query_products = "SELECT * FROM products"
-    orders = pd.read_sql_query(query_orders, engine)
-    products = pd.read_sql_query(query_products, engine)
+    orders = pd.read_sql_query("SELECT id_produto, quantidade_do_produto_vendida FROM orders WHERE data_da_compra >= '2023-05-01' AND data_da_compra < '2023-06-01'", engine)
+    products = pd.read_sql_query("SELECT id_produto, grupo_do_produto FROM products", engine)
     merged_data = pd.merge(orders, products, on='id_produto')
-    faturamento = merged_data.groupby('grupo_do_produto').agg({'preco_unitario': 'sum'}).reset_index()
+    faturamento = merged_data.groupby('grupo_do_produto').sum().reset_index()
     fig, ax = plt.subplots()
-    sns.pieplot(data=faturamento, x='preco_unitario', y='grupo_do_produto', ax=ax)
+    sns.pieplot(data=faturamento, x='quantidade_do_produto_vendida', labels=faturamento['grupo_do_produto'], ax=ax)
     return fig
