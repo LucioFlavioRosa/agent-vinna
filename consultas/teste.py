aa15a7fe988a2c599b5eae48c549007c95aba1b9
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:45:47 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,16 +1,16 @@
 
 import pandas as pd
-import sqlalchemy
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 import os
 
 def estimativa():
-    engine = sqlalchemy.create_engine(os.getenv('banco_sql_postgresql'))
+    engine = create_engine(os.environ['banco_sql_postgresql'])
     query = """
-    SELECT DATE_TRUNC('month', o.data_da_compra) AS mes, SUM(o.preco_unitario * o.quantidade_do_produto_vendida) AS faturamento
-    FROM orders o
-    WHERE o.data_da_compra >= '2023-01-01' AND o.data_da_compra < '2024-01-01'
+    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    FROM orders
+    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
     GROUP BY mes
     ORDER BY mes;
     """
