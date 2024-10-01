78162ce32c52248b03ad04d2adaeb38212feada3
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:30:30 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,23 +1,31 @@
 
 import pandas as pd
-import sqlalchemy
+from sqlalchemy import create_engine
+from prophet import Prophet
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
 def estimativa():
-    engine = sqlalchemy.create_engine(os.getenv('banco_sql_postgresql'))
-    query = """
-    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
-    FROM orders
-    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
-    GROUP BY mes
-    ORDER BY mes;
-    """
-    df = pd.read_sql_query(query, engine)
-    fig, ax = plt.subplots()
-    sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
-    ax.set_title('Faturamento Mensal de 2023 do Grupo Padaria')
-    ax.set_xlabel('Mes')
-    ax.set_ylabel('Faturamento (R$)')
-    return fig
+    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    query_orders = "SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) as faturamento FROM orders WHERE data_da_compra >= '2023-07-01' AND data_da_compra < '2024-08-01' GROUP BY data_da_compra ORDER BY data_da_compra"
+    df_orders = pd.read_sql_query(query_orders, engine)
+    
+    df_orders['data_da_compra'] = pd.to_datetime(df_orders['data_da_compra'])
+    df_orders.rename(columns={'data_da_compra': 'ds', 'faturamento': 'y'}, inplace=True)
+    
+    model = Prophet()
+    model.fit(df_orders)
+    
+    future = model.make_future_dataframe(periods=31)
+    forecast = model.predict(future)
+    
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=forecast, x='ds', y='yhat', label='Estimativa de Faturamento')
+    plt.title('Estimativa de Faturamento para Julho de 2024')
+    plt.xlabel('Data')
+    plt.ylabel('Faturamento')
+    plt.legend()
+    plt.savefig('resultado_python/estimativa_faturamento_julho_2024.png')
+    
+    return 'estimativa_faturamento_julho_2024.png'
