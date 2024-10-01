426a3224e840bbb01f929aa64697a633966eb58b
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:28:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ image_path = "vinna.png"
 st.image(image_path, use_column_width='always')
 
 pergunta = st.text_input("Qual a sua pergunta?")
-if pergunta:
-    
-    #with st.spinner('O agente está buscando a melhor maneira de responder a sua pergunta!...'):
-    response = agent_vinna.main(question=pergunta)
 
-    if type(response) == pd.DataFrame:
-        st.dataframe(response)
+#with st.spinner('O agente está buscando a melhor maneira de responder a sua pergunta!...'):
+response = agent_vinna.main(question=pergunta)
 
-    elif hasattr(response, 'plot'):
-        st.pyplot(response)
-    else:
-        st.write(response)
+if type(response) == pd.DataFrame:
+    st.dataframe(response)
+elif hasattr(response, 'plot'):
+    st.pyplot(response)
+else:
+    st.write(response)
