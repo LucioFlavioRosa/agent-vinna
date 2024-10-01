5b0ee27ef351a58a91e00e6da738e45a1d257cc2
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:32:48 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,14 +1,23 @@
 
 import pandas as pd
-import sqlalchemy
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import os
 
 def estimativa():
- engine = sqlalchemy.create_engine(os.environ['banco_sql_postgresql'])
- orders = pd.read_sql_query("SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento FROM orders WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01' GROUP BY data_da_compra ORDER BY data_da_compra", engine)
- orders['mes'] = orders['data_da_compra'].dt.to_period('M')
- monthly_revenue = orders.groupby('mes')['faturamento'].sum().reset_index()
- fig, ax = plt.subplots()
- sns.barplot(x='mes', y='faturamento', data=monthly_revenue, ax=ax)
- return fig
+    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    query = """
+    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    FROM orders
+    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
+    GROUP BY mes
+    ORDER BY mes;
+    """
+    df = pd.read_sql_query(query, engine)
+    fig, ax = plt.subplots()
+    sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
+    ax.set_title('Faturamento Mensal de 2023')
+    ax.set_xlabel('Mes')
+    ax.set_ylabel('Faturamento')
+    return fig
