181e1fefe0c2a46ae3ccc4b0c522a35198c5ae4e
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-09-30 22:21:07 -0300

    Update leitura_yaml_files.py

--- utilidades/leitura_yaml_files.py
+++ utilidades/leitura_yaml_files.py
@@ -5,9 +5,9 @@ from pathlib import Path
 
 
 def leitura(arquivo: str) -> dict:
-    caminho = Path(__file__).parent
+    caminho = "mount/src/agent-vinna/dado_adicionais/"
     print(caminho)
-    arquivo_completo = caminho /"dado_adicionais" / arquivo
+    arquivo_completo = caminho + arquivo
     if os.path.isfile(arquivo_completo):
         with open(arquivo_completo, 'r') as file:
             dados: dict = yaml.safe_load(file)
