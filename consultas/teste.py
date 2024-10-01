a967fe3c08b10e4a26f27eedef6185358b523493
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:42:14 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,12 +1,12 @@
 
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sqlalchemy import create_engine
+import os
 
 def estimativa():
-    engine = create_engine(os.getenv('banco_sql_postgresql'))
+    engine = create_engine(os.environ['banco_sql_postgresql'])
     query = """
     SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
     FROM orders
