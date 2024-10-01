299ee123ea3824cf6c20a3e3f67c663f79cd2cdd
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 08:56:35 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -10,7 +10,7 @@ def estimativa():
     orders = pd.read_sql_query("SELECT id_produto, quantidade_do_produto_vendida FROM orders WHERE data_da_compra >= '2023-05-01' AND data_da_compra < '2023-06-01'", engine)
     products = pd.read_sql_query("SELECT id_produto, grupo_do_produto FROM products", engine)
     merged_data = pd.merge(orders, products, on='id_produto')
-    faturamento = merged_data.groupby('grupo_do_produto').sum().reset_index()
+    faturamento = merged_data.groupby('grupo_do_produto').agg({'quantidade_do_produto_vendida': 'sum'}).reset_index()
     fig, ax = plt.subplots()
     sns.pieplot(data=faturamento, x='quantidade_do_produto_vendida', labels=faturamento['grupo_do_produto'], ax=ax)
     return fig
