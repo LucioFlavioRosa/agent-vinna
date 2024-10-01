d7c7da93db80b4830d2353ff1132521916d463be
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 08:45:09 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,23 +1,24 @@
 
 import pandas as pd
-import sqlalchemy
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sqlalchemy import create_engine
 import os
 
 def estimativa():
-    engine = sqlalchemy.create_engine(os.getenv('banco_sql_postgresql'))
+    engine = create_engine(os.getenv('banco_sql_postgresql'))
     query = """
-    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
-    FROM orders
-    WHERE DATE_PART('year', data_da_compra) = 2023
+    SELECT EXTRACT(MONTH FROM o.data_da_compra) AS mes, SUM(o.preco_unitario * o.quantidade_do_produto_vendida) AS faturamento
+    FROM orders o
+    JOIN products p ON o.id_produto = p.id_produto
+    WHERE p.grupo_do_produto = 'padaria' AND EXTRACT(YEAR FROM o.data_da_compra) = 2023
     GROUP BY mes
     ORDER BY mes;
     """
     df = pd.read_sql_query(query, engine)
     fig, ax = plt.subplots()
     sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
-    ax.set_title('Faturamento Mensal em 2023')
-    ax.set_xlabel('Mes')
+    ax.set_title('Faturamento Mensal em 2023 do Grupo Padaria')
+    ax.set_xlabel('Mês')
     ax.set_ylabel('Faturamento')
     return fig
