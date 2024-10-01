aaad70708577705151a7b562d0218be6da58dfb6
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:32:02 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -10,12 +10,12 @@ def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
     
     query_orders = "SELECT * FROM orders WHERE data_da_compra >= '2023-07-01' AND data_da_compra < '2023-08-01'"
-    query_products = "SELECT * FROM products"
-    
     orders = pd.read_sql_query(query_orders, engine)
+    
+    query_products = "SELECT * FROM products"
     products = pd.read_sql_query(query_products, engine)
     
-    merged_data = pd.merge(orders, products, on='id_produto')
+    merged_data = orders.merge(products, on='id_produto')
     
     faturamento_julho = merged_data.groupby(['subgrupo_do_produto']) \
                                    .apply(lambda x: (x['preco_unitario'] * x['quantidade_do_produto_vendida']).sum()) \
@@ -23,17 +23,12 @@ def estimativa():
     
     faturamento_julho['subgrupo_do_produto'] = faturamento_julho['subgrupo_do_produto'].astype(str)
     
-    model = Prophet()
-    model.fit(faturamento_julho.rename(columns={'subgrupo_do_produto': 'ds', 'faturamento': 'y'}))
-    
-    future = model.make_future_dataframe(periods=30)
-    forecast = model.predict(future)
-    
+    sns.set_theme(style="whitegrid")
     plt.figure(figsize=(10, 6))
-    sns.barplot(data=faturamento_julho, x='subgrupo_do_produto', y='faturamento')
-    plt.title('Faturamento por Subgrupo - Julho de 2024')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig('resultado_python/faturamento_julho_2024.png')
+    plt.pie(faturamento_julho['faturamento'], labels=faturamento_julho['subgrupo_do_produto'], autopct='%1.1f%%')
+    plt.title('Faturamento de Julho de 2024 por Subgrupo - Grupo Padaria')
     
-    return 'faturamento_julho_2024.png'
+    plt.savefig('resultado_python/grafico_faturamento_julho_2024.png')
+    plt.close()
+    
+    return 'grafico_faturamento_julho_2024.png'
