ea15ad1326ca23a955fe236454ae55c34b87f704
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:04:19 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,20 +1,18 @@
 
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
-import os
 
 def estimativa():
- engine = create_engine(os.getenv('banco_sql_postgresql'))
- query = """
- SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
- FROM orders
- WHERE DATE_PART('year', data_da_compra) = 2023
- GROUP BY mes
- ORDER BY mes;
- """
- df = pd.read_sql_query(query, engine)
- sns.barplot(x='mes', y='faturamento', data=df)
+ engine = create_engine(os.environ['banco_sql_postgresql'])
+ orders = pd.read_sql_query("SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento FROM orders WHERE data_da_compra BETWEEN '2023-01-01' AND '2023-12-31' GROUP BY data_da_compra", engine)
+ orders['mes'] = orders['data_da_compra'].dt.to_period('M')
+ monthly_revenue = orders.groupby('mes')['faturamento'].sum().reset_index()
+ sns.barplot(x='mes', y='faturamento', data=monthly_revenue)
  plt.xticks(rotation=45)
+ plt.title('Faturamento Mensal de 2023')
+ plt.xlabel('Mes')
+ plt.ylabel('Faturamento')
  return plt.show()
