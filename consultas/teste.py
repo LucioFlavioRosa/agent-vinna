d761fbf7f8526e1c8d5b5b5eda6aeaa2123366e5
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 09:19:21 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -1,39 +1,23 @@
 
 import pandas as pd
-from sqlalchemy import create_engine
-from prophet import Prophet
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine
 import os
 
 def estimativa():
     engine = create_engine(os.getenv('banco_sql_postgresql'))
-    
-    query_orders = """
-    SELECT data_da_compra, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
+    query = """
+    SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
     FROM orders
-    WHERE data_da_compra >= '2023-01-01' AND data_da_compra < '2024-01-01'
-    GROUP BY data_da_compra
-    ORDER BY data_da_compra
+    WHERE EXTRACT(YEAR FROM data_da_compra) = 2023
+    GROUP BY mes
+    ORDER BY mes;
     """
-    
-    df_orders = pd.read_sql_query(query_orders, engine)
-    
-    df_orders['data_da_compra'] = pd.to_datetime(df_orders['data_da_compra'])
-    df_orders = df_orders.rename(columns={'data_da_compra': 'ds', 'faturamento': 'y'})
-    
-    model = Prophet()
-    model.fit(df_orders)
-    
-    future = model.make_future_dataframe(periods=180)
-    forecast = model.predict(future)
-    
-    monthly_faturamento = forecast[['ds', 'yhat']].set_index('ds').resample('M').sum()
-    monthly_faturamento.reset_index(inplace=True)
-    
-    plt.figure(figsize=(10, 6))
-    plt.pie(monthly_faturamento['yhat'], labels=monthly_faturamento['ds'].dt.strftime('%Y-%m'), autopct='%1.1f%%')
-    plt.title('Faturamento Mensal Estimado de Janeiro a Julho de 2024')
-    plt.savefig('resultado_python/grafico_faturamento.png')
-    
-    return 'grafico_faturamento.png'
+    df = pd.read_sql_query(query, engine)
+    fig, ax = plt.subplots()
+    sns.barplot(x='mes', y='faturamento', data=df, ax=ax)
+    ax.set_title('Faturamento Mensal de 2023')
+    ax.set_xlabel('Mes')
+    ax.set_ylabel('Faturamento')
+    return fig
