b6f133537d2dbd3bf76afeb0a708495278b83df1
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2024-10-01 01:10:43 -0300

    Update generated file

--- consultas/teste.py
+++ consultas/teste.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
 def estimativa():
- engine = create_engine(os.environ['banco_sql_postgresql'])
+ engine = create_engine(os.getenv('banco_sql_postgresql'))
  query = """
  SELECT DATE_TRUNC('month', data_da_compra) AS mes, SUM(preco_unitario * quantidade_do_produto_vendida) AS faturamento
  FROM orders
@@ -17,4 +17,5 @@ def estimativa():
  df = pd.read_sql_query(query, engine)
  sns.barplot(x='mes', y='faturamento', data=df)
  plt.xticks(rotation=45)
+ plt.title('Faturamento Mensal de 2023')
  return plt.show()
